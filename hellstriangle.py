06021e2c75cc603715c1647345a09349c02b54e0
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-06-05 12:29:30 -0300

    Maximum from top to bottom triangle

--- hellstriangle.py
+++ hellstriangle.py
@@ -1,4 +1,3 @@
-
 def valid_struct_triangle(triangle):
     size = 0
     for array in triangle:
@@ -9,4 +8,15 @@ def valid_struct_triangle(triangle):
 
 
 def max_nearest_element(index, array):
-    return max(array[index], array[index+1])
+    if len(array) > 1:
+        return max(array[index], array[index+1])
+    return array[0]
+
+
+def maximum_top_to_bottom(triangle):
+    index = 0
+    elements = triangle[0]
+    for array in triangle[1:]:
+        elements.append(max_nearest_element(index, array))
+        index = array.index(elements[-1])
+    return sum(elements)
--- tests/test_hellstriangle.py
+++ tests/test_hellstriangle.py
@@ -17,8 +17,19 @@ def test_invalid_struct_triangle():
 @pytest.mark.parametrize("values", [
     [0, [1, 2, 3, 4, 5], 2],
     [1, [1, 2, 3, 4, 5], 3],
-    [2, [1, 2, 3, 4, 5], 4]
+    [2, [1, 2, 3, 4, 5], 4],
+    [0, [1], 1]
 ])
 def test_max_nearest_element(values):
     index, array, result = values
     assert hellstriangle.max_nearest_element(index, array) == result
+
+
+@pytest.mark.parametrize("values", [
+    ([[6], [3, 5], [9, 7, 1], [4, 6, 8, 4]], 26),
+    ([[6], [10, 5], [9, 7, 1], [4, 6, 8, 4]], 31),
+    ([[6], [3, 4], [9, 7, 8], [4, 6, 8, 10]], 28)
+])
+def test_maximum_helltriangle(values):
+    triangle, result = values
+    assert hellstriangle.maximum_top_to_bottom(triangle) == result
